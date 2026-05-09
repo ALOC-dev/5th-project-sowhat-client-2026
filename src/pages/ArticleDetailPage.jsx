a8a582d1c316df9b99dfe2172fcd18728650b014
seochaeun <elamorts309@gmail.com>
2026-05-09 22:29:32 +0900

    페이지 추가

--- src/pages/ArticleDetailPage.jsx
+++ src/pages/ArticleDetailPage.jsx
@@ -1,7 +1,19 @@
 import { useParams } from "react-router-dom";
+import { getArticles } from "../api/articles";
 
 export default function ArticleDetailPage() {
   const { article_id } = useParams();
+  const articles = getArticles();
 
-  return <div>{article_id}</div>;
+  const article = articles.find(
+    (article) => article.article_id === Number(article_id)
+  );
+
+  return (
+    <div>
+      <h1>{article.title}</h1>
+      <p>{article.date}</p>
+      <p>{article.content}</p>
+    </div>
+  );
 }

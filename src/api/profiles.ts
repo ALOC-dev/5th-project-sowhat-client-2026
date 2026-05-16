48e2c49b95443f798b8d0e8f03d2a426d45de0ad
Jiin <jxxniie05@gmail.com>
2026-05-16 21:37:08 +0900

    feat: 사용자 프로필 API 추가 (타입 수정 필요) 	modified:   src/api/profiles.ts

--- src/api/profiles.ts
+++ src/api/profiles.ts
@@ -1,9 +1,40 @@
-export async function getProfile(user_id: number): Promise<void> {}
+import { api } from "./client.ts";
+import { ProfileResponse } from "./contracts.ts";
 
-//  {
-// 	age?: number;
-// 	gender?: string;
-// 	region?: string;
-// 	job?: string;
-// 	interest?: string;
-// }
+export async function createProfile(payload: {
+	age: number;
+	gender: string;
+	region: string;
+	job: string;
+	interest: string;
+}): Promise<ProfileResponse | void> {
+	const data = await api<ProfileResponse>(`/api/profiles`, {
+		method: "POST",
+		body: JSON.stringify(payload),
+	});
+	return data;
+}
+
+export async function getProfile(
+	user_id: number,
+): Promise<ProfileResponse | void> {
+	const data = await api<ProfileResponse>(`/api/profiles/${user_id}`);
+	return data;
+}
+
+export async function modifyProfile(
+	user_id: number,
+	payload: {
+		age?: number;
+		gender?: string;
+		region?: string;
+		job?: string;
+		interest?: string;
+	},
+): Promise<ProfileResponse | void> {
+	const data = await api<ProfileResponse>(`/api/profiles/${user_id}`, {
+		method: "PATCH",
+		body: JSON.stringify(payload),
+	});
+	return data;
+}

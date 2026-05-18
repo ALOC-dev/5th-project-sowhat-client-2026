754b85e1f873faf7299a51ac9712c0983409dddc
Jiin <jxxniie05@gmail.com>
2026-05-19 00:04:58 +0900

    feat: 전체조회/상세조회와 api호출 함수 연동 	modified:   src/pages/Article/ArticleDetailPage.tsx 	modified:   src/pages/Article/ArticleListPage.tsx 	modified:   src/ui/ArticleCard.tsx

--- src/pages/Article/ArticleDetailPage.tsx
+++ src/pages/Article/ArticleDetailPage.tsx
@@ -1,42 +1,61 @@
-import { useNavigate, useParams  } from "react-router-dom";
-import { getArticles } from "../../api/articles";
-import styled from "./ArticleDetailPage.module.css"
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { getArticleDetail } from "../../api/articles";
+import { ArticleDetail } from "../../types";
+import styled from "./ArticleDetailPage.module.css";
 
 export default function ArticleDetailPage() {
-  const { article_id } = useParams();
-  const navigate = useNavigate();
+	const { article_id } = useParams();
+	const navigate = useNavigate();
 
-  const articles = getArticles();
-  const article = articles.find(
-    (article) => article.article_id === Number(article_id)
-  );
+	const [articleDetail, setArticleDetail] = useState<ArticleDetail | null>(
+		null,
+	);
 
-  
-  
-  return (
-    <div className={styled.page}>
-      <button onClick={() => navigate("/articles")}>목록으로</button>
-      
-      <div className={styled.layout}>
-        <article >
-          {!article ?(
-            <p>기사를 찾을 수 없습니다.</p>
-          ) : (
-          <>
-            <h1>{article.title}</h1>
-            <p>{article.date}</p>
-            <p>{article.content}</p>
-          </>
-        )}
-        </article>
+	useEffect(() => {
+		const fetchArticleDetail = async () => {
+			const fetched = await getArticleDetail(Number(article_id));
+			setArticleDetail((prev) => fetched ?? null);
+		};
 
-        <article className={styled.login}>
-            <p className={styled.p1}>이 소식이 나에게 어떤 영향을 줄까요?</p>
-            <p className={styled.p2}>지금 로그인하고 확인하세요.</p>
-            <button>로그인</button>
+		fetchArticleDetail();
+	}, []);
 
-        </article>
-      </div>
-    </div>
-    );
+	return (
+		<div className={styled.page}>
+			<button onClick={() => navigate("/articles")}>목록으로</button>
+
+			<div className={styled.layout}>
+				<article>
+					{articleDetail ? (
+						<>
+							<h1>{articleDetail.title}</h1>
+							<p>
+								{articleDetail.date?.toDateString() ??
+									"2026-05-19"}
+							</p>
+							<p>{articleDetail.media}</p>
+							<p>{articleDetail.content}</p>
+							<p>
+								—————————————————————————————————————————————————
+							</p>
+							<p>{articleDetail.summary}</p>
+							<h3>핵심 키워드</h3>
+							<p>{articleDetail.keyword}</p>
+						</>
+					) : (
+						<p>기사를 찾을 수 없습니다.</p>
+					)}
+				</article>
+
+				<article className={styled.login}>
+					<p className={styled.p1}>
+						이 소식이 나에게 어떤 영향을 줄까요?
+					</p>
+					<p className={styled.p2}>지금 로그인하고 확인하세요.</p>
+					<button>로그인</button>
+				</article>
+			</div>
+		</div>
+	);
 }
--- src/ui/ArticleCard.tsx
+++ src/ui/ArticleCard.tsx
@@ -1,27 +1,34 @@
+import { Article } from "../types";
 import styled from "./ArticleCard.module.css";
 
-type Article = {
-  article_id: number;
-  title: string;
-  date: string;
-  content: string;
-  image: string;
-};
+// type Article = {
+//   article_id: number;
+//   title: string;
+//   date: string;
+//   content: string;
+//   image: string;
+// };
 
 type ArticleCardProps = {
-  article: Article;
-  onDetailView: (articleId: number) => void;
+	article: Article;
+	onDetailView: (articleId: number) => void;
 };
 
-export default function ArticleCard({ article, onDetailView }: ArticleCardProps) {
+export default function ArticleCard({
+	article,
+	onDetailView,
+}: ArticleCardProps) {
 	return (
 		<div
 			className={styled.articleCard}
 			key={article.article_id}
-			onClick={()=>onDetailView(article.article_id)}
+			onClick={() => onDetailView(article.article_id)}
 		>
 			<h2 className={styled.articleTitle}>{article.title}</h2>
-			<p className={styled.articleDate}>{article.date}</p>
+			<p className={styled.articleDate}>
+				{article.date?.toDateString() ?? "2026-05-19"}
+			</p>
+			<p>{article.media}</p>
 			<p className={styled.articleContent}>{article.content}</p>
 		</div>
 	);
